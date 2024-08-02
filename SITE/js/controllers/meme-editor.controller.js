4b19d02b691723e5cf4e392825ff55826b49797e
supaGirl <kerenvasserman.bm@gmail.com>
2024-08-02 20:15:32 +0300

    Build an initial home page

---
+++ SITE/js/controllers/meme-editor.controller.js
@@ -0,0 +1,15 @@
+'use strict'
+
+function onInit() {
+  console.log('welcome')
+}
+
+function onSearchMeme(search){
+    console.log('your search', search)
+}
+
+function onAddTxt(elTxt) {
+  console.log('your txt', elTxt)
+}
+
+
